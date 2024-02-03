23ae22464c6cb0ed7d71e0e309192647d17cebd6
Luis Cabrel Ardian <luisitocard1@gmail.com>
2024-02-02 22:39:28 -0500

    registro

--- app/src/main/java/com/programadoreshuacho/busescolar/MainActivity.kt
+++ app/src/main/java/com/programadoreshuacho/busescolar/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.programadoreshuacho.busescolar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         //Thread.sleep(5000)
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
-        setContentView(R.layout.activity_registro)
+        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_registro)
         //ocultamos el nabvar para esta vista
         supportActionBar?.hide()
+        val btnRegistrar = findViewById<Button>(R.id.btnRegistrar)
+        btnRegistrar.setOnClickListener {
+            startActivity(Intent(this,Registro::class.java))
+        }
     }
 }
--- app/src/main/java/com/programadoreshuacho/busescolar/Registro.kt
+++ app/src/main/java/com/programadoreshuacho/busescolar/Registro.kt
@@ -2,30 +2,44 @@ package com.programadoreshuacho.busescolar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.Toast
+import java.util.*
 
 class Registro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+
         //ocultamos el nabvar para esta vista
         supportActionBar?.hide()
         //https://www.youtube.com/watch?v=nzQVzIHIzUg&ab_channel=Codingraph
-        val spinner = findViewById<Spinner>(R.id.cbo_spinner)
+        val spinner = findViewById<Spinner>(R.id.cbo_tipoDoc)
         //val lista = listOf("Dni","CE","Pasaporte")
         val lista = resources.getStringArray(R.array.opciones)
-        val adaptador = ArrayAdapter(this,android.R.layout.simple_spinner_item,lista)
+
+        Log.d("LISTA: ", Arrays.toString(lista))
+
+        //val adaptador = ArrayAdapter(this,android.R.layout.simple_spinner_item,lista)
+        val adaptador = ArrayAdapter(this,R.layout.spinner_lista_style,lista)
         spinner.adapter = adaptador
 
         spinner.onItemSelectedListener= object :
             AdapterView.OnItemSelectedListener{
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                //TODO("Not yet implemented")
-                Toast.makeText(this@Registro,lista[p2],Toast.LENGTH_LONG).show()
+                //Log.d("PARENT: ", Arrays.toString(p0))
+                //Log.d("VIEW: ", lista[p1])
+                Log.d("POSITION: ", lista[p2])
+                //Log.d("ID: ", lista[p3])
+
+                Toast.makeText(applicationContext,lista[p2],9000).show()
+                Toast.makeText(this@Registro,"mensaje TOAST",Toast.LENGTH_LONG).show()
+
+
             }
 
             override fun onNothingSelected(p0: AdapterView<*>?) {
